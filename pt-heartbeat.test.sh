51b426c4c1718bbb043ff9dff58d6aec74582c9c
knoxvilledba <knoxvilledba@gmail.com>
2012-10-23 14:55:32 -0400

    adding in pt-heartbeat.test.sh

--- pt-heartbeat.test.sh
+++ pt-heartbeat.test.sh
@@ -4,10 +4,11 @@
 #intended to test for replication lag with percona heartbeat and email alert along with diagnostics
 #sample cron: */10 * * * * /root/scripts/pt-heartbeat.test.sh >> /root/scripts/pt-heartbeat.test.log 2>&1
 #
+export OUTFILE=/tmp/mail.pt-heartbeat.out
 
 date
 
-export CHECK=`pt-heartbeat --database=percona --check --master-server-id=101`
+export CHECK=`pt-heartbeat --database=percona --check --master-server-id=x`
 echo $CHECK
 
 export DELAY=`echo $CHECK | awk '$1 > 60'`
@@ -16,10 +17,10 @@ if [ -z "$DELAY" ]
 then
   echo "no delay"
 else
-  echo -e "subject: MySQL replication delay on $HOSTNAME\nto: michaelw436@gmail.com,jacob@thisorthat.com,matt@thisorthat.com\n" > /tmp/mail.pt-heartbeat.out
-  echo "delay is $DELAY" >> /tmp/mail.pt-heartbeat.out
-  mysql -e "show slave status \G" >> /tmp/mail.pt-heartbeat.out
-  mysqladmin processlist >> /tmp/mail.pt-heartbeat.out
-  /usr/sbin/sendmail -t < /tmp/mail.pt-heartbeat.out
+  echo -e "subject: MySQL replication delay on $HOSTNAME\nto: email.recipient@domain.com\n" > $OUTFILE
+  echo "delay is $DELAY" >> $OUTFILE
+  mysql -e "show slave status \G" >> $OUTFILE
+  mysqladmin processlist >> $OUTFILE
+  /usr/sbin/sendmail -t < $OUTFILE
 fi     
 
