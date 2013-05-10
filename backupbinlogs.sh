115774823485d8bf6e2992bc6799bbdc96160d07
knoxvilledba <knoxvilledba@gmail.com>
2013-05-10 14:06:45 -0400

    adding in backupbinlogs.sh

---
+++ backupbinlogs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z $1 ]; then #default to standard location
+ export BINLOGSDIR=/opt/mysql/data/binlogs
+else
+ export BINLOGSDIR=$1
+fi
+
+export BINLOGSINDEX=$BINLOGSDIR/mysql-bin.index
+export BACKUPDIR=/opt/mysql/db_backup/`hostname`/arch_binlogs/
+mkdir -p $BACKUPDIR
+export DATEAPPEND=`date +'%Y%m%d_%H%M%S'`
+
+#rotate the curent log
+mysql -e "flush binary logs"
+
+#cp all binlogs except for current one (last row in index file)
+time for i in $(head -n -1 $BINLOGSINDEX); do 
+   if [ ! -f $BACKUPDIR/`basename $i`.gz ] 
+   then
+      echo $i;
+      gzip -cv $i > $BACKUPDIR/`basename $i`.gz
+   fi
+done
+
+cp -v $BINLOGSINDEX $BACKUPDIR/mysql-bin.index.$DATEAPPEND
+
+mysql -e "purge binary logs before adddate(current_timestamp(), interval -1 day)"
+mysql -e "show binary logs"
+
+#keep 1 week of backups
+find $BACKUPDIR -name "mysql-bin*" -mtime +6 -exec rm {} \;
+
